2a9e1560698612d1896891f85ac8f7b88ac94040
ZhangXingxing <125960373@qq.com>
2022-03-20 20:19:44 +0800

    Initial Commit

---
+++ Algorithm/Algorithm.h
@@ -0,0 +1,18 @@
+//
+//  Algorithm.h
+//  Algorithm
+//
+//  Created by Zhang Xiangxiong on 2022/3/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Algorithm.
+FOUNDATION_EXPORT double AlgorithmVersionNumber;
+
+//! Project version string for Algorithm.
+FOUNDATION_EXPORT const unsigned char AlgorithmVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Algorithm/PublicHeader.h>
+
+
