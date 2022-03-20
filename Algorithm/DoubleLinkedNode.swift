2e4c39c6f45548d412a34a1f0c1b46f598e04294
ZhangXingxing <125960373@qq.com>
2022-03-21 02:36:01 +0800

    add file DoubleLinkedNode

---
+++ Algorithm/DoubleLinkedNode.swift
@@ -0,0 +1,12 @@
+//
+//  DoubleLinkedNode.swift
+//  Algorithm
+//
+//  Created by Zhang Xiangxiong on 2022/3/21.
+//
+
+import Foundation
+
+class DoubleLinkedNode {
+    
+}
---
+++ AlgorithmTests/DoubleLinkedNodeTests.swift
@@ -0,0 +1,12 @@
+//
+//  DoubleLinkedNodeTests.swift
+//  AlgorithmTests
+//
+//  Created by Zhang Xiangxiong on 2022/3/21.
+//
+
+import XCTest
+
+class DoubleLinkedNodeTests: XCTestCase {
+
+}
